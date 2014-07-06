b994e44ade72449186db78ed66e271a607abc73c
Jake Bladt <jake@jakebladt.com>
2014-07-06 17:13:28 -0400

    draw board

--- t3.js
+++ t3.js
@@ -3,8 +3,30 @@ t3.gameView = function() {};
 t3.gameController = function() {};
 
 (function($, gameView, gameController) {
+
+  gameView.getDrawingContext = function() {
+    return gameView.canvas.getContext("2d");
+  };
+
+  gameView.drawLine = function(x1, y1, x2, y2) {
+    var context = gameView.getDrawingContext();
+    context.lineWidth = 10;
+    context.strokeStyle = 'black';
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+  };
+
+  gameView.drawBoard = function() {
+    gameView.drawLine(200, 0, 200, 600);
+    gameView.drawLine(400, 0, 400, 600);
+    gameView.drawLine(0, 200, 600, 200);
+    gameView.drawLine(0, 400, 600, 400);
+  };
+
   $(document).ready(function() {
-    alert("jQuery is loaded.");	
+  	gameView.canvas = $("#gameCanvas")[0];
+    gameView.drawBoard();
   });
 
 })(jQuery, t3.gameView, t3.gameController);
