18e412aa35092be049d8e8b46b4285845fa4a8c2
Jake Bladt <jake@jakebladt.com>
2014-07-06 19:59:42 -0400

    determine winner

--- t3.js
+++ t3.js
@@ -31,10 +31,42 @@ t3.gameView = function() {};
     this.activePlayer.squares.push(ndx);
   };
 
+  gameController.getWinningPlayer = function() {
+  	var ret = null;
+    $(this.winningSets).each(function() {
+      if(gameController.playerHasAll(gameController.player1, this)) ret = gameController.player1;
+      if(gameController.playerHasAll(gameController.player2, this)) ret = gameController.player2;
+    });
+    return ret;
+  };
+
+  gameController.playerHasAll = function(player, squares) {
+  	var ret = true;
+    $(squares).each(function() {
+      var ndx = player.squares.indexOf(parseInt(this));
+      ret = ret && (ndx >= 0);
+    });
+    return ret;
+  };
+
   gameController.gameStatus = function() {
+    winner = this.getWinningPlayer();
+    if(winner) {
+      status = "win";
+      message = winner.symbol + " wins.";
+    } else {
+      if(this.availableSquares.count == 0) {
+      	status = "draw";
+      	message = "It's a draw.";
+      } else {
+      	status = "in progress";
+      	message = this.activePlayer.symbol + "'s turn";
+      }
+    }
+
     return { 
-      winner:  null,
-      message: this.activePlayer.symbol + "'s turn" };
+      status:  status,
+      message: message };
   };
 
   // TODO - These should be scoped to the view.
