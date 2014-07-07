93720a952847821a43a94a41726db286a211ccbd
Jake Bladt <jake@jakebladt.com>
2014-07-06 23:15:06 -0400

    separate controller

--- t3.js
+++ t3.js
@@ -1,98 +1,10 @@
 var t3 = function() {};
-t3.gameController = function() {};
 t3.gameView = function() {};
 
 (function($, gameView, gameController, ai) {
 
   // TODO - Separate out game data from controller.
   ai.gameData = gameController;
-
-  gameController.winningSets = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
-
-  gameController.startNewGame = function() {
-    gameController.availableSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  };
-
-  gameController.startTwoPlayerGame = function() {
-    this.startNewGame();
-    this.player1 = { 
-  	  symbol: "X",
-  	  squares: [] 
-    };
-    this.player2 = { 
-  	  symbol: "O",
-  	  squares: []
-    };
-    this.activePlayer = this.player1;
-  };
-
-  gameController.startOnePlayerGame = function() {
-    this.startNewGame();
-    this.player1 = { 
-  	  symbol: "X",
-  	  squares: [] 
-    };
-    this.player2 = { 
-  	  symbol: "O",
-  	  squares: [],
-  	  ai: ai
-    };
-
-    this.player2.ai.setPlayers(this.player2, this.player1);
-    this.activePlayer = this.player1;
-  };
-
-  gameController.togglePlayer = function() {
-    this.activePlayer = (this.activePlayer === this.player1) ? 
-      this.player2 : this.player1;
-  };
-
-  gameController.claimSquare = function(square) {
-  	var ndx = parseInt(square.index);
-  	this.availableSquares.splice(this.availableSquares.indexOf(ndx), 1);
-    this.activePlayer.squares.push(ndx);
-  };
-
-  gameController.getWinningPlayer = function() {
-  	var ret = null;
-    $(this.winningSets).each(function() {
-      if(gameController.playerHasAll(gameController.player1, this)) ret = gameController.player1;
-      if(gameController.playerHasAll(gameController.player2, this)) ret = gameController.player2;
-    });
-    return ret;
-  };
-
-  gameController.playerHasAll = function(player, squares) {
-  	var ret = true;
-    $(squares).each(function() {
-      ret = ret && (player.squares.indexOf(parseInt(this)) >= 0);
-    });
-    return ret;
-  };
-
-  gameController.gameStatus = function() {
-    winner = this.getWinningPlayer();
-    if(winner) {
-      status = "win";
-      message = winner.symbol + " wins.";
-    } else {
-      if(this.availableSquares.length == 0) {
-      	status = "draw";
-      	message = "It's a draw.";
-      } else {
-      	status = "in progress";
-      	message = this.activePlayer.symbol + "'s turn";
-      }
-    }
-
-    return { 
-      status:  status,
-      message: message };
-  };
 
   // TODO - These should be scoped to the view.
   var BODY_PADDING = 20;
@@ -217,4 +129,4 @@ t3.gameView = function() {};
     gameView.drawBoard();
   });
 
-})(jQuery, t3.gameView, t3.gameController, joshua);
+})(jQuery, t3.gameView, t3Controller, joshua);
---
+++ t3controller.js
@@ -0,0 +1,92 @@
+t3Controller = function() {};
+
+(function(gameController) {
+
+  gameController.winningSets = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  gameController.startNewGame = function() {
+    gameController.availableSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  };
+
+  gameController.startTwoPlayerGame = function() {
+    this.startNewGame();
+    this.player1 = { 
+  	  symbol: "X",
+  	  squares: [] 
+    };
+    this.player2 = { 
+  	  symbol: "O",
+  	  squares: []
+    };
+    this.activePlayer = this.player1;
+  };
+
+  gameController.startOnePlayerGame = function() {
+    this.startNewGame();
+    this.player1 = { 
+  	  symbol: "X",
+  	  squares: [] 
+    };
+    this.player2 = { 
+  	  symbol: "O",
+  	  squares: [],
+  	  ai: ai
+    };
+
+    this.player2.ai.setPlayers(this.player2, this.player1);
+    this.activePlayer = this.player1;
+  };
+
+  gameController.togglePlayer = function() {
+    this.activePlayer = (this.activePlayer === this.player1) ? 
+      this.player2 : this.player1;
+  };
+
+  gameController.claimSquare = function(square) {
+  	var ndx = parseInt(square.index);
+  	this.availableSquares.splice(this.availableSquares.indexOf(ndx), 1);
+    this.activePlayer.squares.push(ndx);
+  };
+
+  gameController.getWinningPlayer = function() {
+  	var ret = null;
+    $(this.winningSets).each(function() {
+      if(gameController.playerHasAll(gameController.player1, this)) ret = gameController.player1;
+      if(gameController.playerHasAll(gameController.player2, this)) ret = gameController.player2;
+    });
+    return ret;
+  };
+
+  gameController.playerHasAll = function(player, squares) {
+  	var ret = true;
+    $(squares).each(function() {
+      ret = ret && (player.squares.indexOf(parseInt(this)) >= 0);
+    });
+    return ret;
+  };
+
+  gameController.gameStatus = function() {
+    winner = this.getWinningPlayer();
+    if(winner) {
+      status = "win";
+      message = winner.symbol + " wins.";
+    } else {
+      if(this.availableSquares.length == 0) {
+      	status = "draw";
+      	message = "It's a draw.";
+      } else {
+      	status = "in progress";
+      	message = this.activePlayer.symbol + "'s turn";
+      }
+    }
+
+    return { 
+      status:  status,
+      message: message };
+  };
+	
+})(t3Controller);
